44a6fe57c0d56fee4e22a9ad69063ac80400244b
AurelieG13 <108137928+AurelieG13@users.noreply.github.com>
2023-09-26 18:34:46 +0200

    commit exo java 7/10 partie 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.example.pojo.Garage;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -10,7 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        Garage garage = new Garage("Chez Michel", "ventabren", 12);
-        System.out.println(garage.getNom());
+        Garage monGarage = createGarage();
+        System.out.println(monGarage);
+    }
+
+    public static Garage createGarage() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez entrer le nom d'un garage :");
+        String nomGarage = sc.nextLine();
+        System.out.println("Veuillez entrer l'adresse du garage :");
+        String adresseGarage = sc.nextLine();
+        System.out.println("Veuillez entrer le nombre de place dans votre garage :");
+        int placeGarage = sc.nextInt();
+
+        return new Garage(nomGarage, adresseGarage, placeGarage);
     }
 }
